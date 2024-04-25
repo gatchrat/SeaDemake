fadc77eb87340b3f7bd085358480d60174d9ae5b
gatchrat <henkolt@t-online.de>
2024-04-25 14:35:13 +0200

    Add basic values to company

--- Assets/Scripts/Company.cs
+++ Assets/Scripts/Company.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Company
+public static class Company
 {
-
+    private static List<Ship> ownedShips;
+    private static List<Contract> openContracts;
+    private static List<Contract> acceptedContracts;
+    public static int curMoney = 0;
 }
